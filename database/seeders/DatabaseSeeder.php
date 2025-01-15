8f1239d3c656fd31309b1342cd9de0b552571218
ForcesR2N <muhammadzuhrizal07@gmail.com>
2025-01-15 22:30:25 +0700

    small changed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -83,7 +83,7 @@ class DatabaseSeeder extends Seeder
             }
         }
 
-        Student::factory(20)
+        Student::factory(50)
             ->withConsistentGradeAndDepartment()
             ->create();
     }
